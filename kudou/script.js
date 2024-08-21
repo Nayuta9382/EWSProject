6173cee491dcda3f41f21dc2d6ff1f1c433a5ee3
kkudou <k.kudou.sys23@morijyobi.ac.jp>
2024-08-21 09:48:50 +0900

    工藤　部品作成

---
+++ kudou/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let submitted = false;
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuButton = document.getElementById('menu-btn');
+    const navMenu = document.getElementById('nav-menu');
+
+    menuButton.addEventListener('click', function() {
+        navMenu.classList.toggle('open');
+        this.classList.toggle('active');
+    });
+});
